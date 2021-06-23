6dd9f8d4c966af2a0aca5804e0b8e09e5b36e9fe
mherman22 <47120265+mhermank@users.noreply.github.com>
2021-06-23 11:27:10 +0300

    update

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+
+const Sequelize = require('sequelize');
+const seq = require('./database/config/config');
 const port = process.env.PORT || 5000;
 
-app.get('/',(req,res)=>{
-    res.send('hello world')
+const sequelize = new Sequelize(seq.development.database, seq.development.username, seq.development.password, {
+  host: 'localhost',
+  dialect: 'postgres'
 });
 
-app.listen(port,()=>{
-    console.log(`server running at ${port}`);
-})
+sequelize.authenticate()
+    .then(() => {
+        console.log('Database Connection has been established successfully !!');
+        sequelize.sync()
+            .then(() => console.log('Database Synced !!'))
+            .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
+
+app.listen(port, () => {
+  console.log(`server running at ${port}`);
+});
